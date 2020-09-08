aade09b0616030d71c43799bed391f34744fad61
Myrmex <dfusi@hotmail.com>
2020-09-08 17:32:14 +0200

    MsSection

---
+++ Cadmus.Itinera.Parts/Codicology/MsCompositionPart.cs
@@ -0,0 +1,55 @@
+﻿using Cadmus.Core;
+using Fusi.Tools.Config;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cadmus.Itinera.Parts.Codicology
+{
+    /// <summary>
+    /// Tag: <c>net.fusisoft.itinera.ms-composition</c>.
+    /// </summary>
+    [Tag("net.fusisoft.itinera.ms-composition")]
+    public sealed class MsCompositionPart : PartBase
+    {
+        public int SheetCount { get; set; }
+        public int GuardSheetCount { get; set; }
+        public List<MsSection> Sections { get; set; }
+
+        /// <summary>
+        /// Get all the key=value pairs (pins) exposed by the implementor.
+        /// </summary>
+        /// <param name="item">The optional item. The item with its parts
+        /// can optionally be passed to this method for those parts requiring
+        /// to access further data.</param>
+        /// <returns>The pins.</returns>
+        public override IEnumerable<DataPin> GetDataPins(IItem item)
+        {
+            // TODO: implement indexing logic...
+            // sample:
+            // return Tag != null
+            //    ? new[]
+            //    {
+            //        CreateDataPin("tag", Tag)
+            //    }
+            //    : Enumerable.Empty<DataPin>();
+        }
+
+        /// <summary>
+        /// Returns a <see cref="String" /> that represents this instance.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="String" /> that represents this instance.
+        /// </returns>
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("[MsComposition]");
+
+            // TODO: append summary data...
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Cadmus.Itinera.Parts/Codicology/MsSection.cs
@@ -0,0 +1,46 @@
+﻿using Fusi.Antiquity.Chronology;
+
+namespace Cadmus.Itinera.Parts.Codicology
+{
+    /// <summary>
+    /// A section of content in a manuscript.
+    /// </summary>
+    public class MsSection
+    {
+        /// <summary>
+        /// Gets or sets the optional tag, used to categorize and group sections.
+        /// </summary>
+        public string Tag { get; set; }
+
+        /// <summary>
+        /// Gets or sets the human-readable label assigned to this section.
+        /// </summary>
+        public string Label { get; set; }
+
+        /// <summary>
+        /// Gets or sets the start location of this section.
+        /// </summary>
+        public MsLocation Start { get; set; }
+
+        /// <summary>
+        /// Gets or sets the end location (inclusive) of this section.
+        /// </summary>
+        public MsLocation End { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date for this section.
+        /// </summary>
+        public HistoricalDate Date { get; set; }
+
+        /// <summary>
+        /// Converts to string.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="string" /> that represents this instance.
+        /// </returns>
+        public override string ToString()
+        {
+            return $"[{Tag} {Label} {Start}-{End}]";
+        }
+    }
+}
