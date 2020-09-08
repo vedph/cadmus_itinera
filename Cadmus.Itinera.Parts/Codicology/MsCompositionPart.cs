101812f663d59d13b62825bd2f212ec138bde28d
Myrmex <dfusi@hotmail.com>
2020-09-08 19:57:06 +0200

    MsCompositionPart

--- Cadmus.Itinera.Parts/Codicology/MsCompositionPart.cs
+++ Cadmus.Itinera.Parts/Codicology/MsCompositionPart.cs
@@ -1,20 +1,40 @@
 ﻿using Cadmus.Core;
 using Fusi.Tools.Config;
-using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Cadmus.Itinera.Parts.Codicology
 {
     /// <summary>
-    /// Tag: <c>net.fusisoft.itinera.ms-composition</c>.
+    /// Manuscript composition part.
+    /// <para>Tag: <c>net.fusisoft.itinera.ms-composition</c></para>.
     /// </summary>
     [Tag("net.fusisoft.itinera.ms-composition")]
     public sealed class MsCompositionPart : PartBase
     {
+        /// <summary>
+        /// Gets or sets the manuscript's sheets count.
+        /// </summary>
         public int SheetCount { get; set; }
+
+        /// <summary>
+        /// Gets or sets the manuscript's guard sheets count.
+        /// </summary>
         public int GuardSheetCount { get; set; }
+
+        /// <summary>
+        /// Gets or sets the manuscript's content sections.
+        /// </summary>
         public List<MsSection> Sections { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MsCompositionPart"/>
+        /// class.
+        /// </summary>
+        public MsCompositionPart()
+        {
+            Sections = new List<MsSection>();
+        }
 
         /// <summary>
         /// Get all the key=value pairs (pins) exposed by the implementor.
@@ -22,24 +42,43 @@ namespace Cadmus.Itinera.Parts.Codicology
         /// <param name="item">The optional item. The item with its parts
         /// can optionally be passed to this method for those parts requiring
         /// to access further data.</param>
-        /// <returns>The pins.</returns>
+        /// <returns>The pins: <c>sheet-count</c>, <c>guard-sheet-count</c>,
+        /// <c>section-count</c>, and a list with keys: <c>section-TAG-count</c>,
+        /// <c>label</c> (filtered, with digits), <c>date-value</c>.</returns>
         public override IEnumerable<DataPin> GetDataPins(IItem item)
         {
-            // TODO: implement indexing logic...
-            // sample:
-            // return Tag != null
-            //    ? new[]
-            //    {
-            //        CreateDataPin("tag", Tag)
-            //    }
-            //    : Enumerable.Empty<DataPin>();
+            DataPinBuilder builder = new DataPinBuilder();
+
+            builder.AddValue("sheet-count", SheetCount);
+            builder.AddValue("guard-sheet-count", GuardSheetCount);
+            builder.AddValue("section-count", Sections?.Count ?? 0);
+
+            if (Sections?.Count > 0)
+            {
+                foreach (MsSection section in Sections)
+                {
+                    if (!string.IsNullOrEmpty(section.Tag))
+                        builder.Increase("section-" + section.Tag, false);
+
+                    if (!string.IsNullOrEmpty(section.Label))
+                    {
+                        builder.AddValue("label",
+                            PinTextFilter.Apply(section.Label, true));
+                    }
+
+                    if (section.Date != null)
+                        builder.AddValue("date-value", section.Date.GetSortValue());
+                }
+            }
+
+            return builder.Build(this);
         }
 
         /// <summary>
-        /// Returns a <see cref="String" /> that represents this instance.
+        /// Converts to string.
         /// </summary>
         /// <returns>
-        /// A <see cref="String" /> that represents this instance.
+        /// A <see cref="string" /> that represents this instance.
         /// </returns>
         public override string ToString()
         {
@@ -47,7 +86,10 @@ namespace Cadmus.Itinera.Parts.Codicology
 
             sb.Append("[MsComposition]");
 
-            // TODO: append summary data...
+            sb.Append("s=").Append(SheetCount);
+            sb.Append(" gs=").Append(GuardSheetCount);
+            if (Sections?.Count > 0)
+                sb.Append(" sct=").Append(Sections.Count);
 
             return sb.ToString();
         }
