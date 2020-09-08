1bb075068f15886e67e2d437c7aa46f821da828f
Myrmex <dfusi@hotmail.com>
2020-09-08 12:37:41 +0200

    EpistLetterInfoPartTest

---
+++ Cadmus.Itinera.Parts.Test/Epistolography/EpistLetterInfoPartTest.cs
@@ -0,0 +1,71 @@
+﻿using Cadmus.Core;
+using Cadmus.Itinera.Parts.Epistolography;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Cadmus.Itinera.Parts.Test.Epistolography
+{
+    public sealed class EpistLetterInfoPartTest
+    {
+        private static EpistLetterInfoPart GetPart()
+        {
+            return new EpistLetterInfoPart
+            {
+                ItemId = Guid.NewGuid().ToString(),
+                RoleId = "some-role",
+                CreatorId = "zeus",
+                UserId = "another",
+                Language = "eng",
+                Subject = "Subject",
+                Heading = "Heading",
+                Note = "A note"
+            };
+        }
+
+        [Fact]
+        public void Part_Is_Serializable()
+        {
+            EpistLetterInfoPart part = GetPart();
+
+            string json = TestHelper.SerializePart(part);
+            EpistLetterInfoPart part2 =
+                TestHelper.DeserializePart<EpistLetterInfoPart>(json);
+
+            Assert.Equal(part.Id, part2.Id);
+            Assert.Equal(part.TypeId, part2.TypeId);
+            Assert.Equal(part.ItemId, part2.ItemId);
+            Assert.Equal(part.RoleId, part2.RoleId);
+            Assert.Equal(part.CreatorId, part2.CreatorId);
+            Assert.Equal(part.UserId, part2.UserId);
+
+            Assert.Equal(part.Language, part2.Language);
+            Assert.Equal(part.Subject, part2.Subject);
+            Assert.Equal(part.Heading, part2.Heading);
+            Assert.Equal(part.Note, part2.Note);
+        }
+
+        [Fact]
+        public void GetDataPins_Ok()
+        {
+            EpistLetterInfoPart part = GetPart();
+
+            List<DataPin> pins = part.GetDataPins(null).ToList();
+
+            Assert.Equal(3, pins.Count);
+
+            DataPin pin = pins.Find(p => p.Name == "language");
+            TestHelper.AssertPinIds(part, pin);
+            Assert.Equal("eng", pin.Value);
+
+            pin = pins.Find(p => p.Name == "subject");
+            TestHelper.AssertPinIds(part, pin);
+            Assert.Equal("subject", pin.Value);
+
+            pin = pins.Find(p => p.Name == "heading");
+            TestHelper.AssertPinIds(part, pin);
+            Assert.Equal("heading", pin.Value);
+        }
+    }
+}
