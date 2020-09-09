6d6b97429c321b3a416ecdd9eb411b41a1689bf0
Myrmex <dfusi@hotmail.com>
2020-09-09 16:34:44 +0200

    MsWatermarksPart

---
+++ Cadmus.Itinera.Parts/Codicology/MsWatermark.cs
@@ -0,0 +1,56 @@
+﻿using Fusi.Antiquity.Chronology;
+using System.Text;
+
+namespace Cadmus.Itinera.Parts.Codicology
+{
+    /// <summary>
+    /// A watermark.
+    /// </summary>
+    public class MsWatermark
+    {
+        /// <summary>
+        /// Gets or sets the watermark's subject. In most cases this will
+        /// be some internal ID, variously mapped to other repositories.
+        /// </summary>
+        public string Subject { get; set; }
+
+        /// <summary>
+        /// Gets or sets the similarity rank, expressing the level of similarity
+        /// of the watermark described here to the paradigmatic watermark model;
+        /// the higher the number, the less similar is the watermark.
+        /// </summary>
+        public int SimilarityRank { get; set; }
+
+        /// <summary>
+        /// Gets or sets the description of this watermark instance.
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the place.
+        /// </summary>
+        public string Place { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date.
+        /// </summary>
+        public HistoricalDate Date { get; set; }
+
+        /// <summary>
+        /// Converts to string.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="string" /> that represents this instance.
+        /// </returns>
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(Subject).Append('=').Append(SimilarityRank);
+
+            if (!string.IsNullOrEmpty(Place)) sb.Append(' ').Append(Place);
+            if (Date != null) sb.Append(' ').Append(Date);
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Cadmus.Itinera.Parts/Codicology/MsWatermarksPart.cs
@@ -0,0 +1,82 @@
+﻿using Cadmus.Core;
+using Fusi.Tools.Config;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Cadmus.Itinera.Parts.Codicology
+{
+    /// <summary>
+    /// Manuscript's watermarks.
+    /// <para>Tag: <c>net.fusisoft.itinera.ms-watermarks</c>.</para>
+    /// </summary>
+    [Tag("net.fusisoft.itinera.ms-watermarks")]
+    public sealed class MsWatermarksPart : PartBase
+    {
+        /// <summary>
+        /// Gets or sets the watermarks.
+        /// </summary>
+        public List<MsWatermark> Watermarks { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MsWatermarksPart"/> class.
+        /// </summary>
+        public MsWatermarksPart()
+        {
+            Watermarks = new List<MsWatermark>();
+        }
+
+        /// <summary>
+        /// Get all the key=value pairs (pins) exposed by the implementor.
+        /// </summary>
+        /// <param name="item">The optional item. The item with its parts
+        /// can optionally be passed to this method for those parts requiring
+        /// to access further data.</param>
+        /// <returns>The pins: <c>tot-count</c>, and a collection of pins with
+        /// keys: <c>subject-X-count</c>, <c>place</c> (filtered, with digits),
+        /// <c>date-value</c>.</returns>
+        public override IEnumerable<DataPin> GetDataPins(IItem item)
+        {
+            DataPinBuilder builder = new DataPinBuilder();
+
+            builder.Set("tot", Watermarks?.Count ?? 0, false);
+
+            if (Watermarks?.Count > 0)
+            {
+                foreach (MsWatermark watermark in Watermarks)
+                {
+                    builder.Increase(watermark.Subject, false, "subject-");
+
+                    if (!string.IsNullOrEmpty(watermark.Place))
+                    {
+                        builder.AddValue("place",
+                            PinTextFilter.Apply(watermark.Place, true));
+                    }
+
+                    if (watermark.Date != null)
+                        builder.AddValue("date-value", watermark.Date.GetSortValue());
+                }
+            }
+
+            return builder.Build(this);
+        }
+
+        /// <summary>
+        /// Converts to string.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="string" /> that represents this instance.
+        /// </returns>
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("[MsWatermarks]");
+
+            sb.Append(string.Join(", ", from watermark in Watermarks
+                                        select watermark.Subject));
+
+            return sb.ToString();
+        }
+    }
+}
