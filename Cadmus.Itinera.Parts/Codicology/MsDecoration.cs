4f3820bad44e1b6caf3821fea9594ab8ccef1a4a
Myrmex <dfusi@hotmail.com>
2020-09-11 14:51:04 +0200

    Decoration submodels

---
+++ Cadmus.Itinera.Parts/Codicology/MsDecoration.cs
@@ -0,0 +1,105 @@
+﻿using Cadmus.Parts.General;
+using System.Collections.Generic;
+
+namespace Cadmus.Itinera.Parts.Codicology
+{
+    /// <summary>
+    /// Manuscript's decoration description.
+    /// </summary>
+    public class MsDecoration
+    {
+        /// <summary>
+        /// Gets or sets the decoration's type.
+        /// </summary>
+        public string Type { get; set; }
+
+        /// <summary>
+        /// Gets or sets a tag used to categorize or group decorations.
+        /// </summary>
+        public string Tag { get; set; }
+
+        /// <summary>
+        /// Gets or sets the decoration's subject.
+        /// </summary>
+        public string Subject { get; set; }
+
+        /// <summary>
+        /// Gets or sets the decoration's color.
+        /// </summary>
+        public string Color { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this decoration is golded.
+        /// </summary>
+        public bool IsGolden { get; set; }
+
+        /// <summary>
+        /// Gets or sets the layout.
+        /// </summary>
+        public string Layout { get; set; }
+
+        /// <summary>
+        /// Gets or sets the tool used for this decoration.
+        /// </summary>
+        public string Tool { get; set; }
+
+        /// <summary>
+        /// Gets or sets the location of the start sheet for this decoration.
+        /// </summary>
+        public MsLocation Start { get; set; }
+
+        /// <summary>
+        /// Gets or sets the location of the end sheet for this decoration.
+        /// </summary>
+        public MsLocation End { get; set; }
+
+        /// <summary>
+        /// Gets or sets the position of the decoration in the page.
+        /// </summary>
+        public string Position { get; set; }
+
+        /// <summary>
+        /// Gets or sets the decoration's 2D size.
+        /// </summary>
+        public PhysicalSize Size { get; set; }
+
+        /// <summary>
+        /// Gets or sets the description.
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the relationship of this decoration with the text.
+        /// </summary>
+        public string TextRelation { get; set; }
+
+        /// <summary>
+        /// Gets or sets the guide letters.
+        /// </summary>
+        public List<MsGuideLetter> GuideLetters { get; set; }
+
+        /// <summary>
+        /// Gets or sets the artist for this decoration.
+        /// </summary>
+        public MsDecorationArtist Artist { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MsDecoration"/> class.
+        /// </summary>
+        public MsDecoration()
+        {
+            GuideLetters = new List<MsGuideLetter>();
+        }
+
+        /// <summary>
+        /// Converts to string.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="string" /> that represents this instance.
+        /// </returns>
+        public override string ToString()
+        {
+            return $"[{Type}] {Subject}, {Color}: {Start} - {End}";
+        }
+    }
+}
---
+++ Cadmus.Itinera.Parts/Codicology/MsDecorationArtist.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+
+namespace Cadmus.Itinera.Parts.Codicology
+{
+    /// <summary>
+    /// An artist of a <see cref="MsDecoration"/>.
+    /// </summary>
+    public class MsDecorationArtist
+    {
+        /// <summary>
+        /// Gets or sets the artist's type.
+        /// </summary>
+        public string Type { get; set; }
+
+        /// <summary>
+        /// Gets or sets the arbitrary identifier internally used for this
+        /// artist.
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the artist's name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets an optional note.
+        /// </summary>
+        public string Note { get; set; }
+
+        /// <summary>
+        /// Gets or sets the sources for this artist.
+        /// </summary>
+        public List<DocReference> Sources { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MsDecorationArtist"/>
+        /// class.
+        /// </summary>
+        public MsDecorationArtist()
+        {
+            Sources = new List<DocReference>();
+        }
+
+        /// <summary>
+        /// Converts to string.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="string" /> that represents this instance.
+        /// </returns>
+        public override string ToString()
+        {
+            return $"[{Type}] {Id} {Name}";
+        }
+    }
+}
