6affbd357e419fe69ebbbfff62cf9533889c6e3a
phylax <dfusi@hotmail.com>
2020-09-18 22:38:37 +0200

    AttachmentsPartSeeder

---
+++ Cadmus.Seed.Itinera.Parts.Test/Epistolography/AttachmentsPartSeederTest.cs
@@ -0,0 +1,58 @@
+﻿using Cadmus.Core;
+using Cadmus.Itinera.Parts.Epistolography;
+using Cadmus.Seed.Itinera.Parts.Epistolography;
+using Fusi.Tools.Config;
+using System;
+using System.Reflection;
+using Xunit;
+
+namespace Cadmus.Seed.Itinera.Parts.Test.Epistolography
+{
+    public sealed class AttachmentsPartSeederTest
+    {
+        private static readonly PartSeederFactory _factory;
+        private static readonly SeedOptions _seedOptions;
+        private static readonly IItem _item;
+
+        static AttachmentsPartSeederTest()
+        {
+            _factory = TestHelper.GetFactory();
+            _seedOptions = _factory.GetSeedOptions();
+            _item = _factory.GetItemSeeder().GetItem(1, "facet");
+        }
+
+        [Fact]
+        public void TypeHasTagAttribute()
+        {
+            Type t = typeof(AttachmentsPartSeeder);
+            TagAttribute attr = t.GetTypeInfo().GetCustomAttribute<TagAttribute>();
+            Assert.NotNull(attr);
+            Assert.Equal("seed.it.vedph.itinera.attachments", attr.Tag);
+        }
+
+        [Fact]
+        public void Seed_Ok()
+        {
+            AttachmentsPartSeeder seeder = new AttachmentsPartSeeder();
+            seeder.SetSeedOptions(_seedOptions);
+
+            IPart part = seeder.GetPart(_item, null, _factory);
+
+            Assert.NotNull(part);
+
+            AttachmentsPart ap = part as AttachmentsPart;
+            Assert.NotNull(ap);
+
+            TestHelper.AssertPartMetadata(ap);
+            Assert.NotNull(ap.Attachments);
+            Assert.NotEmpty(ap.Attachments);
+            foreach (Attachment attachment in ap.Attachments)
+            {
+                Assert.NotNull(attachment.Type);
+                Assert.NotNull(attachment.Name);
+                Assert.NotNull(attachment.Portion);
+                Assert.NotNull(attachment.Note);
+            }
+        }
+    }
+}
---
+++ Cadmus.Seed.Itinera.Parts/Epistolography/AttachmentsPartSeeder.cs
@@ -0,0 +1,51 @@
+﻿using Bogus;
+using Cadmus.Core;
+using Cadmus.Itinera.Parts.Epistolography;
+using Fusi.Tools.Config;
+using System;
+
+namespace Cadmus.Seed.Itinera.Parts.Epistolography
+{
+    /// <summary>
+    /// Attachments part seeder.
+    /// Tag: <c>seed.it.vedph.itinera.attachments</c>.
+    /// </summary>
+    /// <seealso cref="PartSeederBase" />
+    [Tag("seed.it.vedph.itinera.attachments")]
+    public sealed class AttachmentsPartSeeder : PartSeederBase
+    {
+        /// <summary>
+        /// Creates and seeds a new part.
+        /// </summary>
+        /// <param name="item">The item this part should belong to.</param>
+        /// <param name="roleId">The optional part role ID.</param>
+        /// <param name="factory">The part seeder factory. This is used
+        /// for layer parts, which need to seed a set of fragments.</param>
+        /// <returns>A new part.</returns>
+        /// <exception cref="ArgumentNullException">item or factory</exception>
+        public override IPart GetPart(IItem item, string roleId,
+            PartSeederFactory factory)
+        {
+            if (item == null)
+                throw new ArgumentNullException(nameof(item));
+            if (factory == null)
+                throw new ArgumentNullException(nameof(factory));
+
+            AttachmentsPart part = new AttachmentsPart();
+            SetPartMetadata(part, roleId, item);
+
+            for (int n = 1; n <= Randomizer.Seed.Next(1, 4); n++)
+            {
+                part.Attachments.Add(new Faker<Attachment>()
+                    .RuleFor(a => a.Type, f => f.PickRandom("manuscript", "work"))
+                    .RuleFor(a => a.Name, f => f.Lorem.Word())
+                    .RuleFor(a => a.Portion,
+                        f => f.Random.ReplaceNumbers("#.##-#.##"))
+                    .RuleFor(a => a.Note, f => f.Lorem.Sentence())
+                    .Generate());
+            }
+
+            return part;
+        }
+    }
+}
