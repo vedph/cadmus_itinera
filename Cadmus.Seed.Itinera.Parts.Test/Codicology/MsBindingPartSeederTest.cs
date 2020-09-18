94ccaa527ab40dbf5292402b89d6ae54cbb68387
Myrmex <dfusi@hotmail.com>
2020-09-18 18:04:26 +0200

    Seeders infrastructure

---
+++ Cadmus.Seed.Itinera.Parts.Test/Codicology/MsBindingPartSeederTest.cs
@@ -0,0 +1,57 @@
+﻿using Cadmus.Core;
+using Cadmus.Itinera.Parts.Codicology;
+using Cadmus.Seed.Itinera.Parts.Codicology;
+using Fusi.Tools.Config;
+using System;
+using System.Reflection;
+using Xunit;
+
+namespace Cadmus.Seed.Itinera.Parts.Test.Codicology
+{
+    public sealed class MsBindingPartSeederTest
+    {
+        private static readonly PartSeederFactory _factory;
+        private static readonly SeedOptions _seedOptions;
+        private static readonly IItem _item;
+
+        static MsBindingPartSeederTest()
+        {
+            _factory = TestHelper.GetFactory();
+            _seedOptions = _factory.GetSeedOptions();
+            _item = _factory.GetItemSeeder().GetItem(1, "facet");
+        }
+
+        [Fact]
+        public void TypeHasTagAttribute()
+        {
+            Type t = typeof(MsBindingPartSeeder);
+            TagAttribute attr = t.GetTypeInfo().GetCustomAttribute<TagAttribute>();
+            Assert.NotNull(attr);
+            Assert.Equal("seed.it.vedph.itinera.ms-binding", attr.Tag);
+        }
+
+        [Fact]
+        public void Seed_Ok()
+        {
+            MsBindingPartSeeder seeder = new MsBindingPartSeeder();
+            seeder.SetSeedOptions(_seedOptions);
+
+            IPart part = seeder.GetPart(_item, null, _factory);
+
+            Assert.NotNull(part);
+
+            MsBindingPart bp = part as MsBindingPart;
+            Assert.NotNull(bp);
+
+            TestHelper.AssertPartMetadata(bp);
+            Assert.True(bp.Century != 0);
+            Assert.NotNull(bp.Description);
+            Assert.NotNull(bp.CoverMaterial);
+            Assert.NotNull(bp.SupportMaterial);
+            Assert.NotNull(bp.Size);
+            Assert.NotNull(bp.Size.W);
+            Assert.NotNull(bp.Size.D);
+            Assert.NotNull(bp.Size.H);
+        }
+    }
+}
---
+++ Cadmus.Seed.Itinera.Parts/Codicology/MsBindingPartSeeder.cs
@@ -0,0 +1,69 @@
+﻿using Bogus;
+using Cadmus.Core;
+using Cadmus.Itinera.Parts.Codicology;
+using Cadmus.Parts.General;
+using Fusi.Tools.Config;
+using System;
+
+namespace Cadmus.Seed.Itinera.Parts.Codicology
+{
+    /// <summary>
+    /// MsBinding part seeder.
+    /// Tag: <c>seed.it.vedph.itinera.ms-binding</c>.
+    /// </summary>
+    /// <seealso cref="PartSeederBase" />
+    [Tag("seed.it.vedph.itinera.ms-binding")]
+    public sealed class MsBindingPartSeeder : PartSeederBase
+    {
+        /// <summary>
+        /// Creates and seeds a new part.
+        /// </summary>
+        /// <param name="item">The item this part should belong to.</param>
+        /// <param name="roleId">The optional part role ID.</param>
+        /// <param name="factory">The part seeder factory. This is used
+        /// for layer parts, which need to seed a set of fragments.</param>
+        /// <returns>A new part.</returns>
+        /// <exception cref="ArgumentNullException">item or factory</exception>
+        public override IPart GetPart(IItem item, string roleId,
+            PartSeederFactory factory)
+        {
+            if (item == null)
+                throw new ArgumentNullException(nameof(item));
+            if (factory == null)
+                throw new ArgumentNullException(nameof(factory));
+
+            MsBindingPart part = new Faker<MsBindingPart>()
+                .RuleFor(p => p.Century, f => f.Random.Short(8, 15))
+                .RuleFor(p => p.Description, f => f.Lorem.Sentence())
+                .RuleFor(p => p.CoverMaterial,
+                    f => f.PickRandom("parchment", "paper"))
+                .RuleFor(p => p.SupportMaterial,
+                    f => f.PickRandom("parchment", "paper"))
+                .RuleFor(p => p.Size, f =>
+                {
+                    return new PhysicalSize
+                    {
+                        W = new PhysicalDimension
+                        {
+                            Value = f.Random.Float(6, 21),
+                            Unit = "cm"
+                        },
+                        H = new PhysicalDimension
+                        {
+                            Value = f.Random.Float(8, 29),
+                            Unit = "cm"
+                        },
+                        D = new PhysicalDimension
+                        {
+                            Value = f.Random.Float(0.5f, 1.5f),
+                            Unit = "cm"
+                        },
+                    };
+                }).Generate();
+
+            SetPartMetadata(part, roleId, item);
+
+            return part;
+        }
+    }
+}
