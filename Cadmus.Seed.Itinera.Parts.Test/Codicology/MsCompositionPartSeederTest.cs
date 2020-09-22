98c44928569c4f37b0bead001757a56eb6becc15
Myrmex <dfusi@hotmail.com>
2020-09-22 14:11:54 +0200

    MsCompositionPartSeeder

---
+++ Cadmus.Seed.Itinera.Parts.Test/Codicology/MsCompositionPartSeederTest.cs
@@ -0,0 +1,69 @@
+﻿using Cadmus.Core;
+using Cadmus.Itinera.Parts.Codicology;
+using Cadmus.Seed.Itinera.Parts.Codicology;
+using Fusi.Tools.Config;
+using System;
+using System.Reflection;
+using Xunit;
+
+namespace Cadmus.Seed.Itinera.Parts.Test.Codicology
+{
+    public sealed class MsCompositionPartSeederTest
+    {
+        private static readonly PartSeederFactory _factory;
+        private static readonly SeedOptions _seedOptions;
+        private static readonly IItem _item;
+
+        static MsCompositionPartSeederTest()
+        {
+            _factory = TestHelper.GetFactory();
+            _seedOptions = _factory.GetSeedOptions();
+            _item = _factory.GetItemSeeder().GetItem(1, "facet");
+        }
+
+        [Fact]
+        public void TypeHasTagAttribute()
+        {
+            Type t = typeof(MsCompositionPartSeeder);
+            TagAttribute attr = t.GetTypeInfo().GetCustomAttribute<TagAttribute>();
+            Assert.NotNull(attr);
+            Assert.Equal("seed.it.vedph.itinera.ms-composition", attr.Tag);
+        }
+
+        [Fact]
+        public void Seed_Ok()
+        {
+            MsCompositionPartSeeder seeder = new MsCompositionPartSeeder();
+            seeder.SetSeedOptions(_seedOptions);
+
+            IPart part = seeder.GetPart(_item, null, _factory);
+
+            Assert.NotNull(part);
+
+            MsCompositionPart cp = part as MsCompositionPart;
+            Assert.NotNull(cp);
+
+            TestHelper.AssertPartMetadata(cp);
+            Assert.True(cp.SheetCount > 0);
+            Assert.True(cp.GuardSheetCount > 0);
+            Assert.NotEmpty(cp.GuardSheets);
+            Assert.NotEmpty(cp.Sections);
+
+            foreach (MsGuardSheet sheet in cp.GuardSheets)
+            {
+                Assert.NotNull(sheet.Material);
+                Assert.NotNull(sheet.Location);
+                Assert.NotNull(sheet.Date);
+                Assert.NotNull(sheet.Note);
+            }
+
+            foreach (MsSection section in cp.Sections)
+            {
+                Assert.NotNull(section.Label);
+                Assert.NotNull(section.Start);
+                Assert.NotNull(section.End);
+                Assert.NotNull(section.Date);
+            }
+        }
+    }
+}
---
+++ Cadmus.Seed.Itinera.Parts/Codicology/MsCompositionPartSeeder.cs
@@ -0,0 +1,89 @@
+﻿using Bogus;
+using Cadmus.Core;
+using Cadmus.Itinera.Parts.Codicology;
+using Fusi.Antiquity.Chronology;
+using Fusi.Tools.Config;
+using System;
+using System.Globalization;
+
+namespace Cadmus.Seed.Itinera.Parts.Codicology
+{
+    /// <summary>
+    /// Manuscript's composition part seeder.
+    /// Tag: <c>seed.it.vedph.itinera.ms-composition</c>.
+    /// </summary>
+    [Tag("seed.it.vedph.itinera.ms-composition")]
+    public sealed class MsCompositionPartSeeder : PartSeederBase
+    {
+        /// <summary>
+        /// Creates and seeds a new part.
+        /// </summary>
+        /// <param name="item">The item this part should belong to.</param>
+        /// <param name="roleId">The optional part role ID.</param>
+        /// <param name="factory">The part seeder factory. This is used
+        /// for layer parts, which need to seed a set of fragments.</param>
+        /// <returns>A new part.</returns>
+        /// <exception cref="ArgumentNullException">item or factory</exception>
+        public override IPart GetPart(IItem item, string roleId,
+            PartSeederFactory factory)
+        {
+            if (item == null)
+                throw new ArgumentNullException(nameof(item));
+            if (factory == null)
+                throw new ArgumentNullException(nameof(factory));
+
+            MsCompositionPart part = new Faker<MsCompositionPart>()
+                .RuleFor(p => p.SheetCount, f => f.Random.Number(16, 64))
+                .RuleFor(p => p.GuardSheetCount, f => f.Random.Number(1, 4))
+                .Generate();
+            SetPartMetadata(part, roleId, item);
+
+            // guard sheets
+            for (int n = 1; n <= part.GuardSheetCount; n++)
+            {
+                part.GuardSheets.Add(new Faker<MsGuardSheet>()
+                    .RuleFor(s => s.IsBack, n % 2 == 0)
+                    .RuleFor(s => s.Material,
+                        f => f.PickRandom("paper", "parchment"))
+                    .RuleFor(s => s.Location, f => new MsLocation
+                    {
+                        N = n,
+                        V = n % 2 == 0,
+                        L = f.Random.Number(1, 20)
+                    })
+                    .RuleFor(s => s.Date,
+                        HistoricalDate.Parse(("1300" + n)
+                        .ToString(CultureInfo.InvariantCulture)))
+                    .RuleFor(s => s.Note, f => f.Lorem.Sentence())
+                    .Generate());
+            }
+
+            // sections
+            int count = Randomizer.Seed.Next(1, 6);
+            for (int n = 1; n <= count; n++)
+            {
+                part.Sections.Add(new Faker<MsSection>()
+                    .RuleFor(s => s.Tag, f => f.PickRandom(null, f.Lorem.Word()))
+                    .RuleFor(s => s.Label, f => f.Lorem.Sentence(1, 3))
+                    .RuleFor(s => s.Start, f => new MsLocation
+                    {
+                        N = (n - 1) * 2,
+                        V = n % 2 == 0,
+                        L = f.Random.Number(1, 20)
+                    })
+                    .RuleFor(s => s.End, f => new MsLocation
+                    {
+                        N = (n - 1) * 2 + 1,
+                        V = n % 2 == 0,
+                        L = f.Random.Number(1, 20)
+                    })
+                    .RuleFor(s => s.Date,
+                        HistoricalDate.Parse(("1300" + n)
+                            .ToString(CultureInfo.InvariantCulture)))
+                    .Generate());
+            }
+
+            return part;
+        }
+    }
+}
