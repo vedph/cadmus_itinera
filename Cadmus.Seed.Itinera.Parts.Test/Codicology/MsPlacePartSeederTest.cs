39735fee4046e3a384dc1167345631b10a5dd1c6
Myrmex <dfusi@hotmail.com>
2020-09-23 23:46:33 +0200

    MsPlacePartSeeder

---
+++ Cadmus.Seed.Itinera.Parts.Test/Codicology/MsPlacePartSeederTest.cs
@@ -0,0 +1,56 @@
+﻿using Cadmus.Core;
+using Cadmus.Itinera.Parts.Codicology;
+using Cadmus.Seed.Itinera.Parts.Codicology;
+using Fusi.Tools.Config;
+using System;
+using System.Reflection;
+using Xunit;
+
+namespace Cadmus.Seed.Itinera.Parts.Test.Codicology
+{
+    public sealed class MsPlacePartSeederTest
+    {
+        private static readonly PartSeederFactory _factory;
+        private static readonly SeedOptions _seedOptions;
+        private static readonly IItem _item;
+
+        static MsPlacePartSeederTest()
+        {
+            _factory = TestHelper.GetFactory();
+            _seedOptions = _factory.GetSeedOptions();
+            _item = _factory.GetItemSeeder().GetItem(1, "facet");
+        }
+
+        [Fact]
+        public void TypeHasTagAttribute()
+        {
+            Type t = typeof(MsPlacePartSeeder);
+            TagAttribute attr = t.GetTypeInfo().GetCustomAttribute<TagAttribute>();
+            Assert.NotNull(attr);
+            Assert.Equal("seed.it.vedph.itinera.ms-place", attr.Tag);
+        }
+
+        [Fact]
+        public void Seed_Ok()
+        {
+            MsPlacePartSeeder seeder = new MsPlacePartSeeder();
+            seeder.SetSeedOptions(_seedOptions);
+
+            IPart part = seeder.GetPart(_item, null, _factory);
+
+            Assert.NotNull(part);
+
+            MsPlacePart p = part as MsPlacePart;
+            Assert.NotNull(p);
+
+            TestHelper.AssertPartMetadata(p);
+
+            Assert.NotNull(p.Area);
+            Assert.NotNull(p.City);
+            Assert.NotNull(p.Site);
+            Assert.NotNull(p.Subscriber);
+            Assert.NotNull(p.SubscriptionLoc);
+            Assert.NotEmpty(p.Sources);
+        }
+    }
+}
---
+++ Cadmus.Seed.Itinera.Parts/Codicology/MsPlacePartSeeder.cs
@@ -0,0 +1,54 @@
+﻿using Bogus;
+using Cadmus.Core;
+using Cadmus.Itinera.Parts.Codicology;
+using Fusi.Tools.Config;
+using System;
+
+namespace Cadmus.Seed.Itinera.Parts.Codicology
+{
+    /// <summary>
+    /// Manuscript's place part seeder.
+    /// Tag: <c>seed.it.vedph.itinera.ms-place</c>.
+    /// </summary>
+    /// <seealso cref="PartSeederBase" />
+    [Tag("seed.it.vedph.itinera.ms-place")]
+    public sealed class MsPlacePartSeeder : PartSeederBase
+    {
+        /// <summary>
+        /// Creates and seeds a new part.
+        /// </summary>
+        /// <param name="item">The item this part should belong to.</param>
+        /// <param name="roleId">The optional part role ID.</param>
+        /// <param name="factory">The part seeder factory. This is used
+        /// for layer parts, which need to seed a set of fragments.</param>
+        /// <returns>A new part.</returns>
+        /// <exception cref="ArgumentNullException">item or factory</exception>
+        public override IPart GetPart(IItem item, string roleId,
+            PartSeederFactory factory)
+        {
+            if (item == null)
+                throw new ArgumentNullException(nameof(item));
+            if (factory == null)
+                throw new ArgumentNullException(nameof(factory));
+
+            MsPlacePart part = new Faker<MsPlacePart>()
+                .RuleFor(p => p.Area,
+                    f => f.PickRandom("France", "Germany", "Italy"))
+                .RuleFor(p => p.City, f => f.Lorem.Word())
+                .RuleFor(p => p.Site, f => f.Lorem.Word())
+                .RuleFor(p => p.Subscriber, f => f.Lorem.Word())
+                .RuleFor(p => p.SubscriptionLoc, f =>
+                    new MsLocation
+                    {
+                        N = f.Random.Number(20, 60),
+                        V = f.Random.Bool(),
+                        L = f.Random.Number(1, 20)
+                    })
+                .RuleFor(p => p.Sources, SeederHelper.GetDocReferences(1, 3))
+                .Generate();
+            SetPartMetadata(part, roleId, item);
+
+            return part;
+        }
+    }
+}
