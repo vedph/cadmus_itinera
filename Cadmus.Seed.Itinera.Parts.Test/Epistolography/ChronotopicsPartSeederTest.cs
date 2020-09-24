8526eb7c2b7ea3a87ea749193bc9c1af5fbd7be0
Myrmex <dfusi@hotmail.com>
2020-09-24 15:59:25 +0200

    ChronotopicsPartSeeder

---
+++ Cadmus.Seed.Itinera.Parts.Test/Epistolography/ChronotopicsPartSeederTest.cs
@@ -0,0 +1,51 @@
+﻿using Cadmus.Core;
+using Cadmus.Itinera.Parts.Epistolography;
+using Cadmus.Seed.Itinera.Parts.Epistolography;
+using Fusi.Tools.Config;
+using System;
+using System.Reflection;
+using Xunit;
+
+namespace Cadmus.Seed.Itinera.Parts.Test.Epistolography
+{
+    public sealed class ChronotopicsPartSeederTest
+    {
+        private static readonly PartSeederFactory _factory;
+        private static readonly SeedOptions _seedOptions;
+        private static readonly IItem _item;
+
+        static ChronotopicsPartSeederTest()
+        {
+            _factory = TestHelper.GetFactory();
+            _seedOptions = _factory.GetSeedOptions();
+            _item = _factory.GetItemSeeder().GetItem(1, "facet");
+        }
+
+        [Fact]
+        public void TypeHasTagAttribute()
+        {
+            Type t = typeof(ChronotopicsPartSeeder);
+            TagAttribute attr = t.GetTypeInfo().GetCustomAttribute<TagAttribute>();
+            Assert.NotNull(attr);
+            Assert.Equal("seed.it.vedph.itinera.chronotopics", attr.Tag);
+        }
+
+        [Fact]
+        public void Seed_Ok()
+        {
+            ChronotopicsPartSeeder seeder = new ChronotopicsPartSeeder();
+            seeder.SetSeedOptions(_seedOptions);
+
+            IPart part = seeder.GetPart(_item, null, _factory);
+
+            Assert.NotNull(part);
+
+            ChronotopicsPart p = part as ChronotopicsPart;
+            Assert.NotNull(p);
+
+            TestHelper.AssertPartMetadata(p);
+
+            Assert.NotEmpty(p.Chronotopes);
+        }
+    }
+}
---
+++ Cadmus.Seed.Itinera.Parts/Epistolography/ChronotopicsPartSeeder.cs
@@ -0,0 +1,50 @@
+﻿using Bogus;
+using Cadmus.Core;
+using Cadmus.Itinera.Parts.Epistolography;
+using Fusi.Antiquity.Chronology;
+using Fusi.Tools.Config;
+using System;
+
+namespace Cadmus.Seed.Itinera.Parts.Epistolography
+{
+    /// <summary>
+    /// Chronotopics part seeder.
+    /// Tag: <c>seed.it.vedph.itinera.chronotopics</c>.
+    /// </summary>
+    /// <seealso cref="PartSeederBase" />
+    [Tag("seed.it.vedph.itinera.chronotopics")]
+    public sealed class ChronotopicsPartSeeder : PartSeederBase
+    {
+        /// <summary>
+        /// Creates and seeds a new part.
+        /// </summary>
+        /// <param name="item">The item this part should belong to.</param>
+        /// <param name="roleId">The optional part role ID.</param>
+        /// <param name="factory">The part seeder factory. This is used
+        /// for layer parts, which need to seed a set of fragments.</param>
+        /// <returns>A new part.</returns>
+        /// <exception cref="ArgumentNullException">item or factory</exception>
+        public override IPart GetPart(IItem item, string roleId,
+            PartSeederFactory factory)
+        {
+            if (item == null)
+                throw new ArgumentNullException(nameof(item));
+            if (factory == null)
+                throw new ArgumentNullException(nameof(factory));
+
+            ChronotopicsPart part = new ChronotopicsPart();
+            SetPartMetadata(part, roleId, item);
+
+            for (int n = 1; n <= Randomizer.Seed.Next(1, 3 + 1); n++)
+            {
+                part.Chronotopes.Add(new Faker<Chronotope>()
+                    .RuleFor(c => c.Tag, f => f.Lorem.Word())
+                    .RuleFor(c => c.Place, f => f.Lorem.Word())
+                    .RuleFor(c => c.Date, HistoricalDate.Parse($"{1200 + n} AD"))
+                    .Generate());
+            }
+
+            return part;
+        }
+    }
+}
