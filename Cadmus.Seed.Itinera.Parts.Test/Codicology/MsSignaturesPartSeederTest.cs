7fc35b93f5cd73e96f0eea2dbe97530d4519f98a
Myrmex <dfusi@hotmail.com>
2020-09-24 09:21:21 +0200

    MsSignaturesPartSeeder

---
+++ Cadmus.Seed.Itinera.Parts.Test/Codicology/MsSignaturesPartSeederTest.cs
@@ -0,0 +1,51 @@
+﻿using Cadmus.Core;
+using Cadmus.Itinera.Parts.Codicology;
+using Cadmus.Seed.Itinera.Parts.Codicology;
+using Fusi.Tools.Config;
+using System;
+using System.Reflection;
+using Xunit;
+
+namespace Cadmus.Seed.Itinera.Parts.Test.Codicology
+{
+    public sealed class MsSignaturesPartSeederTest
+    {
+        private static readonly PartSeederFactory _factory;
+        private static readonly SeedOptions _seedOptions;
+        private static readonly IItem _item;
+
+        static MsSignaturesPartSeederTest()
+        {
+            _factory = TestHelper.GetFactory();
+            _seedOptions = _factory.GetSeedOptions();
+            _item = _factory.GetItemSeeder().GetItem(1, "facet");
+        }
+
+        [Fact]
+        public void TypeHasTagAttribute()
+        {
+            Type t = typeof(MsSignaturesPartSeeder);
+            TagAttribute attr = t.GetTypeInfo().GetCustomAttribute<TagAttribute>();
+            Assert.NotNull(attr);
+            Assert.Equal("seed.it.vedph.itinera.ms-signatures", attr.Tag);
+        }
+
+        [Fact]
+        public void Seed_Ok()
+        {
+            MsSignaturesPartSeeder seeder = new MsSignaturesPartSeeder();
+            seeder.SetSeedOptions(_seedOptions);
+
+            IPart part = seeder.GetPart(_item, null, _factory);
+
+            Assert.NotNull(part);
+
+            MsSignaturesPart p = part as MsSignaturesPart;
+            Assert.NotNull(p);
+
+            TestHelper.AssertPartMetadata(p);
+
+            Assert.NotEmpty(p.Signatures);
+        }
+    }
+}
