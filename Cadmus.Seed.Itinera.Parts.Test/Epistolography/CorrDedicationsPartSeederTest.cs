48926040df8efb32388365264bd2ff9ad07096ab
Myrmex <dfusi@hotmail.com>
2020-09-25 08:19:37 +0200

    CorrDedicationsPartSeeder

---
+++ Cadmus.Seed.Itinera.Parts.Test/Epistolography/CorrDedicationsPartSeederTest.cs
@@ -0,0 +1,51 @@
+﻿using Cadmus.Core;
+using Cadmus.Itinera.Parts.Epistolography;
+using Cadmus.Seed.Itinera.Parts.Epistolography;
+using Fusi.Tools.Config;
+using System;
+using System.Reflection;
+using Xunit;
+
+namespace Cadmus.Seed.Itinera.Parts.Test.Epistolography
+{
+    public sealed class CorrDedicationsPartSeederTest
+    {
+        private static readonly PartSeederFactory _factory;
+        private static readonly SeedOptions _seedOptions;
+        private static readonly IItem _item;
+
+        static CorrDedicationsPartSeederTest()
+        {
+            _factory = TestHelper.GetFactory();
+            _seedOptions = _factory.GetSeedOptions();
+            _item = _factory.GetItemSeeder().GetItem(1, "facet");
+        }
+
+        [Fact]
+        public void TypeHasTagAttribute()
+        {
+            Type t = typeof(CorrDedicationsPartSeeder);
+            TagAttribute attr = t.GetTypeInfo().GetCustomAttribute<TagAttribute>();
+            Assert.NotNull(attr);
+            Assert.Equal("seed.it.vedph.itinera.corr-dedications", attr.Tag);
+        }
+
+        [Fact]
+        public void Seed_Ok()
+        {
+            CorrDedicationsPartSeeder seeder = new CorrDedicationsPartSeeder();
+            seeder.SetSeedOptions(_seedOptions);
+
+            IPart part = seeder.GetPart(_item, null, _factory);
+
+            Assert.NotNull(part);
+
+            CorrDedicationsPart p = part as CorrDedicationsPart;
+            Assert.NotNull(p);
+
+            TestHelper.AssertPartMetadata(p);
+
+            Assert.NotEmpty(p.Dedications);
+        }
+    }
+}
---
+++ Cadmus.Seed.Itinera.Parts/Epistolography/CorrDedicationsPartSeeder.cs
@@ -0,0 +1,52 @@
+﻿using Bogus;
+using Cadmus.Core;
+using Cadmus.Itinera.Parts.Epistolography;
+using Fusi.Antiquity.Chronology;
+using Fusi.Tools.Config;
+using System;
+
+namespace Cadmus.Seed.Itinera.Parts.Epistolography
+{
+    /// <summary>
+    /// Seeder for the correspondent/author dedications part.
+    /// Tag: <c>seed.it.vedph.itinera.corr-dedications</c>.
+    /// </summary>
+    /// <seealso cref="PartSeederBase" />
+    [Tag("seed.it.vedph.itinera.corr-dedications")]
+    public sealed class CorrDedicationsPartSeeder : PartSeederBase
+    {
+        /// <summary>
+        /// Creates and seeds a new part.
+        /// </summary>
+        /// <param name="item">The item this part should belong to.</param>
+        /// <param name="roleId">The optional part role ID.</param>
+        /// <param name="factory">The part seeder factory. This is used
+        /// for layer parts, which need to seed a set of fragments.</param>
+        /// <returns>A new part.</returns>
+        /// <exception cref="ArgumentNullException">item or factory</exception>
+        public override IPart GetPart(IItem item, string roleId,
+            PartSeederFactory factory)
+        {
+            if (item == null)
+                throw new ArgumentNullException(nameof(item));
+            if (factory == null)
+                throw new ArgumentNullException(nameof(factory));
+
+            CorrDedicationsPart part = new CorrDedicationsPart();
+            SetPartMetadata(part, roleId, item);
+
+            for (int n = 1; n <= Randomizer.Seed.Next(1, 3 + 1); n++)
+            {
+                part.Dedications.Add(new Faker<CorrDedication>()
+                    .RuleFor(d => d.Title, f => f.Lorem.Sentence(1, 3))
+                    .RuleFor(d => d.Date, HistoricalDate.Parse($"{1200 + n} AD"))
+                    .RuleFor(d => d.Date, HistoricalDate.Parse($"{1201 + n} AD"))
+                    .RuleFor(d => d.IsByAuthor, f => f.Random.Bool())
+                    .RuleFor(d => d.Sources, SeederHelper.GetDocReferences(1, 3))
+                    .Generate());
+            }
+
+            return part;
+        }
+    }
+}
