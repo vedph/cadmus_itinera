26cc62a18abe85ec440f60eb848957464ece85ed
Myrmex <dfusi@hotmail.com>
2020-12-08 12:32:26 +0100

    Updated packages

--- InstallToFeed.bat
+++ InstallToFeed.bat
@@ -2,7 +2,7 @@
 echo PRESS ANY KEY TO INSTALL Cadmus Libraries TO LOCAL NUGET FEED
 echo Remember to generate the up-to-date package.
 pause
-c:\exe\nuget add .\Cadmus.Itinera.Parts\bin\Debug\Cadmus.Itinera.Parts.1.0.6.nupkg -source C:\Projects\_NuGet
-c:\exe\nuget add .\Cadmus.Itinera.Services\bin\Debug\Cadmus.Itinera.Services.1.0.10.nupkg -source C:\Projects\_NuGet
-c:\exe\nuget add .\Cadmus.Seed.Itinera.Parts\bin\Debug\Cadmus.Seed.Itinera.Parts.1.0.10.nupkg -source C:\Projects\_NuGet
+c:\exe\nuget add .\Cadmus.Itinera.Parts\bin\Debug\Cadmus.Itinera.Parts.1.0.7.nupkg -source C:\Projects\_NuGet
+c:\exe\nuget add .\Cadmus.Itinera.Services\bin\Debug\Cadmus.Itinera.Services.1.0.11.nupkg -source C:\Projects\_NuGet
+c:\exe\nuget add .\Cadmus.Seed.Itinera.Parts\bin\Debug\Cadmus.Seed.Itinera.Parts.1.0.11.nupkg -source C:\Projects\_NuGet
 pause
