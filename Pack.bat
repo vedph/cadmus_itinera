a9043e0a27f1eef3f410a9676614f73e6d724522
Myrmex <dfusi@hotmail.com>
2021-03-16 22:00:43 +0100

    Version numbers

--- Pack.bat
+++ Pack.bat
@@ -1,6 +1,13 @@
 @echo off
 echo BUILD Cadmus Itinera Packages
+del .\Cadmus.Itinera.Parts\bin\Debug\*.snupkg
 del .\Cadmus.Itinera.Parts\bin\Debug\*.nupkg
+
+del .\Cadmus.Itinera.Services\bin\Debug\*.snupkg
+del .\Cadmus.Itinera.Services\bin\Debug\*.nupkg
+
+del .\Cadmus.Seed.Itinera.Parts\bin\Debug\*.snupkg
+del .\Cadmus.Seed.Itinera.Parts\bin\Debug\*.nupkg
 
 cd .\Cadmus.Itinera.Parts
 dotnet pack -c Debug -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
